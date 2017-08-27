92c8a44f446c4d3f574e78fb3bda8ab925bf673d
boldowa <boldowa.q@gmail.com>
2017-08-28 00:58:33 +0900

    Build tool

---
+++ build/build.sh
@@ -0,0 +1,24 @@
+#/bin/env sh
+########################################
+# build script
+########################################
+
+cd `dirname "$0"`
+
+if test -r Makefile ; then
+	echo "Cleaning..."
+	make clean
+fi
+
+which cmake
+if test ! $? ; then
+	echo "Error: cmake isn't installed. pls install it."
+	exit
+fi
+
+rm -rf CMake* cmake_*
+
+echo "Build start..."
+cmake ..; cmake ..; make
+sync
+
