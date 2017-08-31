0178931c30965ab47c9b6cbe9b81a70782971cba
boldowa <boldowa.q@gmail.com>
2017-08-31 23:43:01 +0900

    compiler types

--- include/common/types.h
+++ include/common/types.h
@@ -3,6 +3,7 @@
  */
 #pragma once
 
+#include "compiler.h"
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
---
+++ include/compiler.h
@@ -0,0 +1,3 @@
+#pragma once
+#define C_COMPILER "(empty)"
+#define C_COMPILER_VER "0"
